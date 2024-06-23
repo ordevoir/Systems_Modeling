32fd4e0ac4bfe78a4947393b54d9ec923482a445
ordevoir <wernadsky@gmail.com>
2024-06-23 03:48:24 +0300

    update

--- PettingZoo/Simplest-Env_with_PyGame/simplest_env.py
+++ PettingZoo/Simplest-Env_with_PyGame/simplest_env.py
@@ -3,14 +3,22 @@ import numpy as np
 from copy import copy
 from pettingzoo import ParallelEnv
 from gymnasium.spaces import Discrete, MultiDiscrete
+import pygame
+
+GREY = (70, 70, 70)
+DARKGREY = (50, 50, 50)
+LIGHTGREY = (80, 80, 80)
+RED = (173, 78, 78)
+GREEN = (78, 173, 78)
 
 class raw_env(ParallelEnv):
     metadata = {
         "name": "simplest_env",
-        "render_modes": ["human", "rgb_array"]
+        "render_modes": ["human", "rgb_array"],
+        "render_fps": 3,
     }
 
-    def __init__(self, grid_size=7, render_mode=None):
+    def __init__(self, grid_size=8, screen_size=320, render_mode=None):
         # entity positions:
         self.escape_y = None
         self.escape_x = None
@@ -22,6 +30,8 @@ class raw_env(ParallelEnv):
         self.grid_size = grid_size
         self.timestep = None
         self.possible_agents = ["prisoner", "guard"]
+        self.render_mode = render_mode
+        self.screen_size = screen_size
     
     def reset(self, seed=None, options=None):
         self.agents = copy(self.possible_agents)
@@ -48,6 +58,14 @@ class raw_env(ParallelEnv):
         # Get dummy infos. Necessary for proper parallel_to_aec conversion
         infos = {a: {} for a in self.agents}
 
+        # prepare PyGame:
+        if self.render_mode == "human":
+            pygame.init()
+            self.screen = None  # поверхность для отрисовки 
+            self.screen_size = (self.screen_size // self.grid_size) * self.grid_size
+            self.clock = pygame.time.Clock()
+            pygame.display.set_caption("Simplest Environment")
+
         return observations, infos
     
     def step(self, actions):
@@ -56,41 +74,41 @@ class raw_env(ParallelEnv):
 
         if prisoner_action == 0 and self.prisoner_x > 0:
             self.prisoner_x -= 1
-        elif prisoner_action == 1 and self.prisoner_x < 6:
+        elif prisoner_action == 1 and self.prisoner_x < self.grid_size-1:
             self.prisoner_x += 1
         elif prisoner_action == 2 and self.prisoner_y > 0:
             self.prisoner_y -= 1
-        elif prisoner_action == 3 and self.prisoner_y < 6:
+        elif prisoner_action == 3 and self.prisoner_y < self.grid_size-1:
             self.prisoner_y += 1
 
         if guard_action == 0 and self.guard_x > 0:
             self.guard_x -= 1
-        elif guard_action == 1 and self.guard_x < 6:
+        elif guard_action == 1 and self.guard_x < self.grid_size-1:
             self.guard_x += 1
         elif guard_action == 2 and self.guard_y > 0:
             self.guard_y -= 1
-        elif guard_action == 3 and self.guard_y < 6:
+        elif guard_action == 3 and self.guard_y < self.grid_size-1:
             self.guard_y += 1
 
         # Generate action masks
         prisoner_action_mask = np.ones(4, dtype=np.int8)
         if self.prisoner_x == 0:
             prisoner_action_mask[0] = 0  # Block left movement
-        elif self.prisoner_x == 6:
+        elif self.prisoner_x == self.grid_size-1:
             prisoner_action_mask[1] = 0  # Block right movement
         if self.prisoner_y == 0:
             prisoner_action_mask[2] = 0  # Block down movement
-        elif self.prisoner_y == 6:
+        elif self.prisoner_y == self.grid_size-1:
             prisoner_action_mask[3] = 0  # Block up movement
 
         guard_action_mask = np.ones(4, dtype=np.int8)
         if self.guard_x == 0:
             guard_action_mask[0] = 0
-        elif self.guard_x == 6:
+        elif self.guard_x == self.grid_size-1:
             guard_action_mask[1] = 0
         if self.guard_y == 0:
             guard_action_mask[2] = 0
-        elif self.guard_y == 6:
+        elif self.guard_y == self.grid_size-1:
             guard_action_mask[3] = 0
 
         # Action mask to prevent guard from going over escape cell
@@ -126,9 +144,9 @@ class raw_env(ParallelEnv):
 
         # Get observations
         observation = (
-                self.prisoner_x + 7 * self.prisoner_y,
-                self.guard_x + 7 * self.guard_y,
-                self.escape_x + 7 * self.escape_y,
+                self.prisoner_x + self.grid_size * self.prisoner_y,
+                self.guard_x + self.grid_size * self.guard_y,
+                self.escape_x + self.grid_size * self.escape_y,
             )
         observations = {
             "prisoner": {
@@ -146,19 +164,55 @@ class raw_env(ParallelEnv):
 
         if any(terminations.values()) or all(truncations.values()):
             self.agents = []
+        
+        if self.render_mode == "human":
+            self.render()
 
         return observations, rewards, terminations, truncations, infos
 
     def render(self):
-        grid = np.full((7, 7), " ")
-        grid[self.prisoner_y, self.prisoner_x] = "P"
-        grid[self.guard_y, self.guard_x] = "G"
-        grid[self.escape_y, self.escape_x] = "E"
-        print(f"{grid} \n")
+        if self.screen is None:
+            self.screen = pygame.display.set_mode((self.screen_size, self.screen_size))
+        self.draw()
+        pygame.display.flip()
+        self.clock.tick(self.metadata["render_fps"])
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.agents = []
+                pygame.quit()
+
+
+    def draw(self):
+        self.screen.fill(GREY)
+        length = self.screen_size
+        step = length // (self.grid_size)
+        radius = 0.4 * length // (self.grid_size)
+        # draw guard and prisoner:
+        red_circle_x = (self.guard_x) * step + step // 2
+        red_circle_y = (self.guard_y) * step + step // 2
+        pygame.draw.circle(self.screen, RED, (red_circle_x, red_circle_y), radius)
+        green_circle_x = (self.prisoner_x) * step + step // 2
+        green_circle_y = (self.prisoner_y) * step + step // 2
+        pygame.draw.circle(self.screen, GREEN, (green_circle_x, green_circle_y), radius)
+        # draw escape:
+        blue_rect_x = self.escape_x * step
+        blue_rect_y = self.escape_y * step
+        pygame.draw.rect(self.screen, DARKGREY, (blue_rect_x, blue_rect_y, step, step))
+        # draw grid:
+        for i in range(1, self.grid_size):
+            pygame.draw.line(self.screen, LIGHTGREY, (0, i*step), (length, i*step))
+        for i in range(1, self.grid_size):
+            pygame.draw.line(self.screen, LIGHTGREY, (i*step, 0), (i*step, length))
+
+    def close(self):
+        if self.screen is not None:
+            pygame.quit()
+            self.screen = None
 
     @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
-        return MultiDiscrete([7 * 7] * 3)
+        return MultiDiscrete([self.grid_size * self.grid_size] * 3)
     
     @functools.lru_cache(maxsize=None)
     def action_space(self, agent):
@@ -167,5 +221,10 @@ class raw_env(ParallelEnv):
 from pettingzoo.test import parallel_api_test
 
 if __name__ == "__main__":
-    env = raw_env()
-    parallel_api_test(env, num_cycles=1_000_000)
+    parallel_env = raw_env(render_mode="human", grid_size=6)
+    # parallel_api_test(env, num_cycles=1_000_000)
+    observations, infos = parallel_env.reset(seed=42)
+    while parallel_env.agents:
+        actions = {agent: parallel_env.action_space(agent).sample() for agent in parallel_env.agents}
+        observations, rewards, terminations, truncations, infos = parallel_env.step(actions)
+    # parallel_env.close()
