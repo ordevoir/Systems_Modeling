22c3f4bd691dd8634d3938a518f5a23fb5fbd2c4
ordevoir <wernadsky@gmail.com>
2024-06-26 00:09:38 +0300

    update

--- PettingZoo/Prisoner-Guard-Prompter/prisoner-guard-prompter.py
+++ PettingZoo/Prisoner-Guard-Prompter/prisoner-guard-prompter.py
@@ -44,7 +44,7 @@ class Escape:
 
 
 class raw_env():
-    def __init__(self, channel_length=8):
+    def __init__(self, channel_length=8, max_cycles=150, render_mode=None):
         self.channel_length = channel_length
         self.prisoner = Prisoner(channel_length)
         self.guard = Guard()
@@ -54,9 +54,14 @@ class raw_env():
         self.possible_agents = [self.prisoner.name, 
                                 self.guard.name,
                                 self.prompter.name]
-        
+        self.dist_thres = 0.1
+        self.max_cycles = max_cycles
+        self.timestep = None
+
     def reset(self, seed=None):
-        self.agens = copy(self.possible_agents)
+        self.agents = copy(self.possible_agents)
+        self.timestep = 0
+
         if seed:
             np.random.seed(seed)
         self.prisoner.position = np.random.uniform(-10, 10, 2).astype(np.float32)
@@ -66,19 +71,24 @@ class raw_env():
         self.prisoner.velocity = np.random.uniform( -2,  2, 2).astype(np.float32)
         self.guard.velocity =    np.random.uniform( -2,  2, 2).astype(np.float32)
 
+        observations = self.get_observations()
+        infos = {self.prisoner.name: {}, self.prompter.name: {}}
+        return observations, infos
+    
+    def get_observations(self):
         observations = {
-            self.prisoner.name: np.concatenate(
+            self.prisoner.name: np.concatenate((
                                 self.prisoner.velocity, 
                                 self.prompter.position - self.prisoner.position,
                                 self.prompter.message,
-                                ),
-            self.prompter.name: np.concatenate(
+                                )),
+            self.prompter.name: np.concatenate((
                                 self.guard.position - self.prompter.position,
                                 self.escape.position - self.prompter.position,
-                                ) 
+                                )) 
         }
-        infos = {self.prisoner.name: {}, self.prompter.name: {}}
-        return observations, infos
+        return observations
+
     
     def step(self, actions):
         prisoner_action = actions["prisoner"]
@@ -100,9 +110,44 @@ class raw_env():
         self.guard.accelerate(force, dt=self.dt)
         self.guard.move(dt=self.dt)
 
-        
+        terminations = {a: False for a in self.agents}
+        rewards = {a: 0 for a in self.agents}
 
-        truncations = {a: False for a in self.agens}
+        if self.distance(self.prisoner, self.escape) < self.dist_thres:
+            rewards[self.prisoner.name] = 1 
+            rewards[self.prompter.name] = 1
+            rewards[self.guard.name] = -1
+            terminations = {a: True for a in self.agents}
+
+        if self.distance(self.prisoner, self.guard) < self.dist_thres:
+            rewards[self.prisoner.name] = -1 
+            rewards[self.prompter.name] = -1
+            rewards[self.guard.name] = 1
+            terminations = {a: True for a in self.agents}
+
+        truncations = {a: False for a in self.agents}
+        if self.timestep > self.max_cycles:
+            truncations = {a: True for a in self.agents}
+            self.agents = []
+        self.timestep += 1
+
+        observations = self.get_observations()
+        infos = {self.prisoner.name: {}, self.prompter.name: {}}
+        return observations, rewards, terminations, truncations, infos
+
+    @staticmethod
+    def distance(entity_1, entity_2):
+        return np.sqrt(np.sum((entity_1.position - entity_2.position)**2))
+
+    # def render(self)
 
 
 
+if __name__ == "__main__":
+    parallel_env = raw_env()
+    # parallel_api_test(parallel_env, num_cycles=1_000_000)
+    observations, infos = parallel_env.reset(seed=42)
+    while parallel_env.agents:
+        actions = {agent: parallel_env.action_space(agent).sample() for agent in parallel_env.agents}
+        observations, rewards, terminations, truncations, infos = parallel_env.step(actions)
+    parallel_env.close()
